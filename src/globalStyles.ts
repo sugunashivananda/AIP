4a613dedff0bc1ea563f8ee1c465cd19f3bb11ec
sugunashivananda <sugunashivananda@gmail.com>
2026-01-14 18:45:41 +0530

    feat: add global styles with styled-components

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -9,9 +9,10 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: ${({ theme }) => theme.typography.fontFamily};
     font-size: ${({ theme }) => theme.typography.fontSize};
+    font-weight: ${({ theme }) => theme.typography.fontWeightRegular};
     line-height: ${({ theme }) => theme.typography.lineHeight};
-    background-color: ${({ theme }) => theme.colors.background};
     color: ${({ theme }) => theme.colors.textPrimary};
+    background-color: ${({ theme }) => theme.colors.background};
   }
 
   a {
@@ -19,17 +20,31 @@ export const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
-  a:hover, a:focus {
-    text-decoration: underline;
-    outline: none;
+  a:focus, button:focus {
+    outline: 2px solid ${({ theme }) => theme.colors.primary};
+    outline-offset: 2px;
   }
 
   button {
     font-family: inherit;
+    font-size: inherit;
+    cursor: pointer;
+    border: none;
+    background: none;
   }
 
-  :focus-visible {
-    outline: 2px solid ${({ theme }) => theme.colors.accent};
-    outline-offset: 2px;
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  th, td {
+    text-align: left;
+    padding: ${({ theme }) => theme.spacing(1)};
+    border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+  }
+
+  th {
+    font-weight: ${({ theme }) => theme.typography.fontWeightBold};
   }
 `;
