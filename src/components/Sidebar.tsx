f2922e1462cf99c86f5f1cfafa92a8323c551c60
sugunashivananda <sugunashivananda@gmail.com>
2026-01-14 18:15:31 +0530

    feat: add Sidebar component with semantic HTML and styled-components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,62 +2,66 @@ import React from 'react';
 import styled from 'styled-components';
 import { sidebarMenu } from '../data/mockData';
 
-const SidebarContainer = styled.aside`
-  background-color: ${({ theme }) => theme.colors.white};
-  width: 250px;
-  padding: ${({ theme }) => theme.spacing.lg};
-  border-right: 1px solid ${({ theme }) => theme.colors.neutralLight};
-  height: 100vh;
+const SidebarContainer = styled.nav`
+  width: 240px;
+  background-color: ${({ theme }) => theme.colors.surface};
+  padding: ${({ theme }) => theme.spacing(3)} 0;
   display: flex;
   flex-direction: column;
+  border-right: 1px solid ${({ theme }) => theme.colors.border};
+  height: 100vh;
 `;
 
 const MenuList = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
-  display: flex;
-  flex-direction: column;
-  gap: ${({ theme }) => theme.spacing.md};
 `;
 
-const MenuItem = styled.li``;
+const MenuItem = styled.li`
+  margin: 0;
+`;
 
-const MenuButton = styled.button`
-  background: none;
-  border: none;
-  color: ${({ theme }) => theme.colors.neutral};
-  font-size: ${({ theme }) => theme.typography.fontSize.md};
-  font-weight: ${({ theme }) => theme.typography.fontWeight.medium};
+const MenuLink = styled.a`
+  display: flex;
+  align-items: center;
+  padding: ${({ theme }) => theme.spacing(2)} ${({ theme }) => theme.spacing(3)};
+  color: ${({ theme }) => theme.colors.textSecondary};
+  font-weight: ${({ theme }) => theme.typography.fontWeightRegular};
+  text-decoration: none;
   cursor: pointer;
-  width: 100%;
-  text-align: left;
-  padding: ${({ theme }) => theme.spacing.sm} 0;
+  transition: background-color 0.2s ease;
 
-  &:focus {
-    outline: 2px solid ${({ theme }) => theme.colors.primaryLight};
+  &:hover, &:focus {
+    background-color: ${({ theme }) => theme.colors.primaryLight};
+    color: ${({ theme }) => theme.colors.surface};
+    outline: none;
+  }
+
+  &:focus-visible {
+    outline: 2px solid ${({ theme }) => theme.colors.accent};
     outline-offset: 2px;
   }
 
-  &:hover {
-    color: ${({ theme }) => theme.colors.primary};
+  & > span {
+    margin-right: ${({ theme }) => theme.spacing(2)};
+    font-size: 1.25rem;
   }
 `;
 
-const Sidebar: React.FC = () => {
+export const Sidebar: React.FC = () => {
   return (
-    <SidebarContainer aria-label="Sidebar navigation">
+    <SidebarContainer aria-label="Main navigation">
       <MenuList>
-        {sidebarMenu.map((item) => (
-          <MenuItem key={item.id}>
-            <MenuButton type="button" aria-label={item.label}>
-              {item.label}
-            </MenuButton>
+        {sidebarMenu.map(({ id, label, icon, href }) => (
+          <MenuItem key={id}>
+            <MenuLink href={href} tabIndex={0}>
+              <span aria-hidden="true">{icon}</span>
+              {label}
+            </MenuLink>
           </MenuItem>
         ))}
       </MenuList>
     </SidebarContainer>
   );
 };
-
-export default Sidebar;
