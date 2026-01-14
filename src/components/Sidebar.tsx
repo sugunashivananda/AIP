bb5c30a8573e291b920748cfb770dc47001fd3ea
sugunashivananda <sugunashivananda@gmail.com>
2026-01-14 17:19:52 +0530

    feat: add Sidebar component with styled components and mock data

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import { sidebarMenu } from '../data/mockData';
 
-const SidebarWrapper = styled.nav`
-  width: 240px;
-  background-color: ${({ theme }) => theme.colors.secondary};
-  color: white;
+const SidebarContainer = styled.aside`
+  background-color: ${({ theme }) => theme.colors.white};
+  width: 250px;
+  padding: ${({ theme }) => theme.spacing.lg};
+  border-right: 1px solid ${({ theme }) => theme.colors.neutralLight};
   height: 100vh;
-  padding: ${({ theme }) => theme.spacing.lg} 0;
   display: flex;
   flex-direction: column;
 `;
@@ -16,37 +16,47 @@ const MenuList = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
-`;
-
-const MenuItem = styled.li`
-  padding: ${({ theme }) => theme.spacing.md} ${({ theme }) => theme.spacing.lg};
-  cursor: pointer;
   display: flex;
-  align-items: center;
-  font-weight: ${({ theme }) => theme.typography.fontWeight.medium};
-
-  &:hover, &:focus {
-    background-color: ${({ theme }) => theme.colors.primary};
-    outline: none;
-  }
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.md};
 `;
 
-const Icon = styled.span`
-  margin-right: ${({ theme }) => theme.spacing.sm};
+const MenuItem = styled.li``;
+
+const MenuButton = styled.button`
+  background: none;
+  border: none;
+  color: ${({ theme }) => theme.colors.neutral};
+  font-size: ${({ theme }) => theme.typography.fontSize.md};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.medium};
+  cursor: pointer;
+  width: 100%;
+  text-align: left;
+  padding: ${({ theme }) => theme.spacing.sm} 0;
+
+  &:focus {
+    outline: 2px solid ${({ theme }) => theme.colors.primaryLight};
+    outline-offset: 2px;
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
 `;
 
 const Sidebar: React.FC = () => {
   return (
-    <SidebarWrapper aria-label="Main Navigation">
+    <SidebarContainer aria-label="Sidebar navigation">
       <MenuList>
-        {sidebarMenu.map(({ id, label, icon, link }) => (
-          <MenuItem key={id} tabIndex={0} role="link" aria-label={label} onClick={() => window.location.href = link} onKeyPress={(e) => { if (e.key === 'Enter') window.location.href = link; }}>
-            <Icon aria-hidden="true">{icon}</Icon>
-            {label}
+        {sidebarMenu.map((item) => (
+          <MenuItem key={item.id}>
+            <MenuButton type="button" aria-label={item.label}>
+              {item.label}
+            </MenuButton>
           </MenuItem>
         ))}
       </MenuList>
-    </SidebarWrapper>
+    </SidebarContainer>
   );
 };
 
