9318aacac68354e18603d0b92a8e924515c45f66
sugunashivananda <sugunashivananda@gmail.com>
2026-01-14 13:46:38 +0530

    Add Header component with styled components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: white;
+  padding: ${({ theme }) => theme.spacing.md};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled.h1`
+  font-size: ${({ theme }) => theme.typography.fontSize.lg};
+  margin: 0;
+`;
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <h1>React TypeScript Project</h1>
-    </header>
+    <HeaderWrapper>
+      <Title>Invoice Dashboard</Title>
+    </HeaderWrapper>
   );
 };
 
