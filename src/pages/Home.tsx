1e867410e394a36a2d479b2bdf29ec65ad4abe4e
sugunashivananda <sugunashivananda@gmail.com>
2026-01-14 13:41:43 +0530

    Add example files for pages, hooks, utils, services, types, public, and tests

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Welcome to the Home Page</h2>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
