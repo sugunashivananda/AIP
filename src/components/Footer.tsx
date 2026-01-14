db13d7c7613fc6a5c1ecbc5040315bbef60d5db0
sugunashivananda <sugunashivananda@gmail.com>
2026-01-14 18:46:13 +0530

    feat: add Footer component with styled-components and accessibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,19 @@ import styled from 'styled-components';
 
 const FooterContainer = styled.footer`
   background-color: ${({ theme }) => theme.colors.surface};
+  border-top: 1px solid ${({ theme }) => theme.colors.border};
   padding: ${({ theme }) => theme.spacing(2)};
   text-align: center;
   font-size: 0.875rem;
   color: ${({ theme }) => theme.colors.textSecondary};
-  border-top: 1px solid ${({ theme }) => theme.colors.border};
 `;
 
-export const Footer: React.FC = () => {
+const Footer: React.FC = () => {
   return (
     <FooterContainer>
-      &copy; {new Date().getFullYear()} Invoice Dashboard. All rights reserved.
+      &copy; {new Date().getFullYear()} Your Company. All rights reserved.
     </FooterContainer>
   );
 };
+
+export default Footer;
